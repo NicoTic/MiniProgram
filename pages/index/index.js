e5651c293886f501842384cc9f05370f1a0e4de9
蒋小泉 <jiangxq@travelsky.com>
2018-11-04 09:58:19 +0800

    新闻列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,33 +8,31 @@ const segmentsList = [
   { segment_id: 3, title: "娱乐", en_title: "yl", active: false },
   { segment_id: 4, title: "军事", en_title: "js", active: false },
   { segment_id: 5, title: "体育", en_title: "ty", active: false },
-  { segment_id: 6, title: "其他", en_title: "other", active: false},
-  {segment_id: 6, title: "其他", en_title: "other", active: false} ,
-  {segment_id: 6, title: "其他", en_title: "other", active: false }
+  { segment_id: 6, title: "其他", en_title: "other", active: false}
 ];
 var currentSectionIndex = 0;
 
 Page({
   data: {
     segmentsList: segmentsList,
-    imgUrls: [{image:'/images/toutiao_news_first.jpg',title:'独家对话胡一天:不介意被定义为"流量"偶像'},
-      {image: '/images/toutiao_news_second.jpg', title:"重庆公交坠江原因公布:乘客与司机互殴致车辆失控"},
-      { image: '/images/toutiao_news_thrid.jpg', title:"重庆公交坠江原因公布:乘客与司机互殴致车辆失控"},
-      { image: '/images/toutiao_news_four.jpg', title:"重庆公交坠江原因公布:乘客与司机互殴致车辆失控"},
-      { image: '/images/toutiao_news_five.jpg', title:"重庆公交坠江原因公布:乘客与司机互殴致车辆失控"},
-      { image: '/images/toutiao_news_six.jpg', title:"重庆公交坠江原因公布:乘客与司机互殴致车辆失控"}],
+    imgUrls: [{ image: '/images/toutiao_news_first.jpg', title: '独家对话胡一天:不介意被定义为"流量"偶像' },
+    { image: '/images/toutiao_news_second.jpg', title: "重庆公交坠江原因公布:乘客与司机互殴致车辆失控" },
+    { image: '/images/toutiao_news_thrid.jpg', title: "重庆公交坠江原因公布:乘客与司机互殴致车辆失控" },
+    { image: '/images/toutiao_news_four.jpg', title: "重庆公交坠江原因公布:乘客与司机互殴致车辆失控" },
+    { image: '/images/toutiao_news_five.jpg', title: "重庆公交坠江原因公布:乘客与司机互殴致车辆失控" },
+    { image: '/images/toutiao_news_six.jpg', title: "重庆公交坠江原因公布:乘客与司机互殴致车辆失控" }],
     indicatorDots: true,
     autoplay: false,
     interval: 5000,
     duration: 1300,
     articlesList: []
   },
-  onLoad(){
+  onLoad() {
     this.loadArticles(0);
   },
 
   onPullDownRefresh() {
-    this.loadArticles(0,()=>{
+    this.loadArticles(0, () => {
       wx.stopPullDownRefresh();
     });
   },
@@ -47,9 +45,9 @@ Page({
       if (segmentsList[i]['segment_id'] == sid) {
         segmentsList[i]['active'] = true
         currentSectionIndex = i
-      }else{
+      } else {
         segmentsList[i]['active'] = false
-      } 
+      }
     }
     this.setData({
       segmentsList: segmentsList
@@ -57,7 +55,7 @@ Page({
     this.loadArticles(sid);
   },
 
-  loadArticles(section_id,callback){
+  loadArticles(section_id, callback) {
     //获取新闻类型
     let new_type = segmentsList[section_id]["en_title"];
     console.log(new_type);
@@ -67,13 +65,13 @@ Page({
       data: {
         type: new_type
       },
-      success: res=>{
+      success: res => {
         let result = res.data.result;
         console.log(result);
-        if(result && result.length){
+        if (result && result.length) {
           //截取日期字符串
-          result.forEach(function(item){
-            item.date = item.date.substring(0,10);
+          result.forEach(function (item) {
+            item.date = item.date.substring(0, 10);
           })
           //设置新闻列表数据
           this.setData({
@@ -81,10 +79,10 @@ Page({
           })
         }
       },
-      fail: ()=>{
+      fail: () => {
         console.log("onFail");
       },
-      complete: ()=>{
+      complete: () => {
         callback && callback();
       }
     })
@@ -97,5 +95,6 @@ Page({
       url: '/pages/detail/detail?article_id=' + aid,
     })
   },
+
 
 })

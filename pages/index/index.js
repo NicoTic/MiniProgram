7f8703093b54499d4063db47cd08f237f0873692
蒋小泉 <jiangxq@travelsky.com>
2018-11-04 09:46:01 +0800

    滑动切换

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,9 +8,7 @@ const segmentsList = [
   { segment_id: 3, title: "娱乐", en_title: "yl", active: false },
   { segment_id: 4, title: "军事", en_title: "js", active: false },
   { segment_id: 5, title: "体育", en_title: "ty", active: false },
-  { segment_id: 6, title: "其他", en_title: "other", active: false},
-  {segment_id: 6, title: "其他", en_title: "other", active: false} ,
-  {segment_id: 6, title: "其他", en_title: "other", active: false }
+  { segment_id: 6, title: "其他", en_title: "other", active: false}
 ];
 var currentSectionIndex = 0;
 
@@ -23,14 +21,37 @@ Page({
       { image: '/images/toutiao_news_four.jpg', title:"重庆公交坠江原因公布:乘客与司机互殴致车辆失控"},
       { image: '/images/toutiao_news_five.jpg', title:"重庆公交坠江原因公布:乘客与司机互殴致车辆失控"},
       { image: '/images/toutiao_news_six.jpg', title:"重庆公交坠江原因公布:乘客与司机互殴致车辆失控"}],
-    indicatorDots: true,
-    autoplay: false,
-    interval: 5000,
-    duration: 1300,
-    articlesList: []
+    articlesList: [],
+    scrollLeft: 0, //tab标题的滚动条位置
+    currentTab: 0, //预设当前项的值
   },
   onLoad(){
     this.loadArticles(0);
+  },
+
+  // 滚动切换标签样式
+  switchTab(e) {
+    let sid = e.detail.current;
+    this.freshenSegmentList(sid);
+    this.setData({
+      currentTab: sid,
+      segmentsList: segmentsList
+    });
+    this.loadArticles(sid);
+    this.checkCor();
+  },
+
+  //判断当前滚动超过一屏时，设置tab标题滚动条。
+  checkCor(){
+    if (this.data.currentTab > 4) {
+      this.setData({
+        scrollLeft: 300
+      })
+    } else {
+      this.setData({
+        scrollLeft: 0
+      })
+    }
   },
 
   onPullDownRefresh() {
@@ -40,21 +61,25 @@ Page({
   },
 
   onSectionClicked(event) {
-    console.log(event);
-    let sid = event.currentTarget.dataset.sid;
+    var sid = event.currentTarget.dataset.sid;
+    this.freshenSegmentList(sid);
+    this.setData({
+      segmentsList: segmentsList
+    });
+    this.loadArticles(sid);
+  },
+
+  //刷新新闻类别列表方法
+  freshenSegmentList(sid){
     //刷新新闻类别选中状态
     for (var i in segmentsList) {
       if (segmentsList[i]['segment_id'] == sid) {
         segmentsList[i]['active'] = true
         currentSectionIndex = i
-      }else{
+      } else {
         segmentsList[i]['active'] = false
-      } 
+      }
     }
-    this.setData({
-      segmentsList: segmentsList
-    });
-    this.loadArticles(sid);
   },
 
   loadArticles(section_id,callback){
@@ -86,10 +111,15 @@ Page({
       },
       complete: ()=>{
         callback && callback();
+        //动态设置swiper高度（每条新闻的高度*新闻的条数）
+        var calc = 100 * this.data.articlesList.length * 2;
+        this.setData({
+          winHeight: calc
+        });
       }
     })
   },
-
+  //点击新闻列表，进入新闻详情
   onArticleClicked(event) {
     var aid = event.currentTarget.dataset.aid;
     console.log(event);
